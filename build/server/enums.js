87d93662aafc3b139bda5b6c52e87919da0eed2f
Nicolas Bolanos <nicolas.bolanos@arroyoconsulting.net>
2023-01-30 02:25:27 -0500

    delete build folder

--- build/server/enums.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.Type = void 0
-let Type;
-(function (Type) {
-  Type.Credit = 'credit'
-  Type.Debit = 'debit'
-})(Type = exports.Type || (exports.Type = {}))
--- build/server/types.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.Type = void 0
-let Type;
-(function (Type) {
-  Type.Debit = 'debit'
-  Type.Credit = 'credit'
-})(Type = exports.Type || (exports.Type = {}))
