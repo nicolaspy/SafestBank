aeddf9f045b69099f985fc653e3b5dbc789b6dd1
Nicolas Bolanos <nicolas.bolanos@arroyoconsulting.net>
2023-02-01 01:59:03 -0500

    Add missing cypress tests

--- client/bank_challenge/cypress/e2e/transaction.cy.ts
+++ client/bank_challenge/cypress/e2e/transaction.cy.ts
@@ -1,3 +1,6 @@
+import { Balance } from "../../src/types";
+import { getNumbertoAmount } from "../../src/utlis/common";
+
 describe("transaction view", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/");
@@ -20,31 +23,60 @@ describe("transaction view", () => {
 describe("when logged in", () => {
   describe("transaction view displays the correct information of the current logged in User", () => {
     it("displays the correct balance of the current logged in User", () => {
-      // cy.intercept("http://localhost:5173/api/transactions", {
-      //   fixture: "/transactionsData.json",
-      // });
-      const id = "1";
-      cy.visit("http://localhost:5173/");
+      cy.fixture("balanceData.json").then((fixture: Balance) => {
+        const id = fixture.personId.toString();
+        cy.intercept("GET", `http://localhost:5173/api/balance/${id}`, fixture);
+        cy.intercept("GET", `http://localhost:5173/api/transactions/${id}`, {
+          fixture: "transactionsData.json",
+        });
+        cy.visit("http://localhost:5173/");
 
-      cy.get('input[placeholder="Identification"').type(id);
-      cy.get('input[placeholder="Password"').type(id);
-      cy.contains("Login").click();
+        cy.get('input[placeholder="Identification"').type(id);
+        cy.get('input[placeholder="Password"').type(id);
 
-      cy.request({
-        method: "GET",
-        url: `/api/balance/${id}`,
-      }).then((response) => {
-        expect(response.body).to.eq("1000");
+        cy.contains("Login").click();
+
+        cy.contains(`Balance is: $${getNumbertoAmount(fixture.balance)}`);
       });
     });
   });
-  describe("transaction view displays the correct information of the current logged in User", () => {
+  describe("when a user wants to withdraw more than his current balance", () => {
+    it("displays Insufficient funds as error message", () => {
+      cy.fixture("balanceData.json").then((fixture: Balance) => {
+        const id = fixture.personId.toString();
+        cy.intercept("GET", `http://localhost:5173/api/balance/${id}`, fixture);
+        cy.intercept("GET", `http://localhost:5173/api/transactions/${id}`, {
+          fixture: "transactionsData.json",
+        });
+
+        cy.visit("http://localhost:5173/");
+
+        cy.get('input[placeholder="Identification"').type(id);
+        cy.get('input[placeholder="Password"').type(id);
+        cy.contains("Login").click();
+        cy.contains("Withdraw").click();
+        cy.get("#debit_amount").type("99999999{enter}", { force: true });
+        cy.contains("Insufficient funds");
+      });
+    });
+  });
+  describe("when a user wants to withdraw Lless than his current balance", () => {
     it("displays the correct balance of the current logged in User", () => {
-      cy.request({
-        method: "GET",
-        url: "http://localhost:5173/api/transactions/1",
-      }).then((response) => {
-        expect(response.body).to.eq("1000");
+      cy.fixture("balanceData.json").then((fixture: Balance) => {
+        const id = fixture.personId.toString();
+        cy.intercept("GET", `http://localhost:5173/api/balance/${id}`, fixture);
+        cy.intercept("GET", `http://localhost:5173/api/transactions/${id}`, {
+          fixture: "transactionsData.json",
+        });
+
+        cy.visit("http://localhost:5173/");
+
+        cy.get('input[placeholder="Identification"').type(id);
+        cy.get('input[placeholder="Password"').type(id);
+        cy.contains("Login").click();
+        cy.contains("Withdraw").click();
+        cy.get("#debit_amount").type("99999999{enter}", { force: true });
+        cy.contains("Insufficient funds");
       });
     });
   });
